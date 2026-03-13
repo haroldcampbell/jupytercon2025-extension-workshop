dd76c481498b559a1407a29c7330cffc4de3381a
Harold Campbell <harold.campbell@gmail.com>
2026-03-13 12:08:31 -0500

    Test development loop with a simple change

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin
+	JupyterFrontEnd,
+	JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
 import { requestAPI } from './request';
@@ -9,22 +9,23 @@ import { requestAPI } from './request';
  * Initialization data for the jupytercon2025-extension-workshop extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupytercon2025-extension-workshop:plugin',
-  description: 'A JupyterLab extension that displays a random image and caption.',
-  autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupytercon2025-extension-workshop is activated!');
+	id: 'jupytercon2025-extension-workshop:plugin',
+	description: 'HMC: A JupyterLab extension: random image generator',
+	autoStart: true,
+	activate: (app: JupyterFrontEnd) => {
+		console.log('HMC Extension is activated!');
 
-    requestAPI<any>('hello')
-      .then(data => {
-        console.log(data);
-      })
-      .catch(reason => {
-        console.error(
-          `The jupytercon2025_extension_workshop server extension appears to be missing.\n${reason}`
-        );
-      });
-  }
+		requestAPI<any>('hello')
+			.then(data => {
+				// console.log(data);
+				console.log('HELLO API: HMC Extension is activated!');
+			})
+			.catch(reason => {
+				console.error(
+					`The jupytercon2025_extension_workshop server extension appears to be missing.\n${reason}`
+				);
+			});
+	}
 };
 
 export default plugin;
